4144c6e19b5129626b54b789b40f618292410470
TTXXSS <1374230455@qq.com>
2019-07-15 19:03:38 +0800

    refactor:method getBasePrice getDiscountPrice and getShippingPrice in class PriceCalculator

--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -1,10 +1,32 @@
 package com.tws.refactoring.extract_variable;
 
 public class PriceCalculator {
-    double getPrice(int quantity, int itemPrice) {
+    private final int QUANTITY_BOUND = 500;
+    private final double FIVE_DISCOUNT = 0.05;
+    private final double ONE_DISCOUNT = 0.1;
+    private final double UP_LIMIT = 100.0;
+
+    public double getPrice(int quantity, int itemPrice) {
         // Price consists of: base price - discount + shipping cost
-        return quantity * itemPrice -
-                Math.max(0, quantity - 500) * itemPrice * 0.05 +
-                Math.min(quantity * itemPrice * 0.1, 100.0);
+        double basePrice = getBasePrice(quantity, itemPrice);
+        double discountPrice = getDiscountPrice(quantity, itemPrice);
+        double shippingCost = getShipingPrice(quantity, itemPrice);
+        return basePrice - discountPrice + shippingCost;
+
+    }
+
+    public double getBasePrice(int quantity, int itemPrice) {
+        double basePrice = quantity * itemPrice;
+        return basePrice;
+    }
+
+    public double getDiscountPrice(int quantity, int itemPrice) {
+        double discountPrice = Math.max(0, quantity - QUANTITY_BOUND) * itemPrice * FIVE_DISCOUNT;
+        return discountPrice;
+    }
+
+    public double getShipingPrice(int quantity, int itemPrice) {
+        double shippingCost = Math.min(quantity * itemPrice * ONE_DISCOUNT, UP_LIMIT);
+        return shippingCost;
     }
 }
