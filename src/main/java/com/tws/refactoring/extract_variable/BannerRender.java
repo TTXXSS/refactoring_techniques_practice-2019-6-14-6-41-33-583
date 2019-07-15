21dc2581cafe5158a1b181eac77fd824573fdce0
TTXXSS <1374230455@qq.com>
2019-07-15 18:43:01 +0800

    refactor:method renderBanner in class BannerRender

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -1,11 +1,12 @@
 package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
-    String renderBanner(String platform, String browser) {
-        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-                (browser.toUpperCase().indexOf("IE") > -1)) {
-            return "IE on Mac?";
-        }
-        return "banner";
+    public String renderBanner(String platform, String browser) {
+        String upperCaseOfPlatform = platform.toUpperCase();
+        String upperCaseOfBrowser =browser.toUpperCase();
+
+        return ((upperCaseOfPlatform.indexOf("MAC") > -1) &&
+                (upperCaseOfBrowser.indexOf("IE") > -1))?"IE on Mac?":"banner";
+
     }
 }
